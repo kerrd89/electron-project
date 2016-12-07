527876739351141199aa937e7721e9fcd41b6728
Peter Springer <peterspringer829@gmail.com>
2016-12-07 12:15:28 -0700

    create database file

---
+++ app/db.js
@@ -0,0 +1,21 @@
+require('sqlite3');
+
+const database = require('knex')({
+  client: 'sqlite3',
+  connection: {
+    filename: './db.sqlite'
+  }
+});
+
+database.schema.hasTable('notes').then((exists) => {
+  if (!exists) {
+    return database.schema.createTable('notes', (t) => {
+      t.increments('id').primary();
+      t.string('title', 100);
+      t.text('body');
+      t.string('created_at');
+    });
+  }
+});
+
+module.exports = database;
